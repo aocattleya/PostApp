363e83fcdd0fdfb61dee040781c1d6f327af26ff
aocattleya <aocattleya@yahoo.co.jp>
2019-10-22 17:25:45 +0900

    add: Posting function

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+
+  def index
+    # @post = Post.find(1)
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+    def post_params
+      params.permit(:content)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
 end
-
-# Rails.application.routes.draw do
-#   [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]'
-# end
